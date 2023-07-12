a1da829be3dc4afaa4e311d84dbce842d160a9a2
gunwook <taeme0912@naver.com>
2023-07-12 13:43:22 +0900

    init commit

---
+++ src/main/java/com/sparta/gunwooklv4/Gunwooklv4Application.java
@@ -0,0 +1,13 @@
+package com.sparta.gunwooklv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gunwooklv4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gunwooklv4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/gunwooklv4/Gunwooklv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gunwooklv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gunwooklv4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
